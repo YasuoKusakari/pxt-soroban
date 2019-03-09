50bbb29f8391c66851297fd37e64525360667272
YasuoKusakari <46615777+YasuoKusakari@users.noreply.github.com>
2019-03-09 12:54:05 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -3,20 +3,21 @@ namespace soroban {
     //% blockId=show_number block="かずを ひょうじ %num"
     export function showNumber(num: number = 0): void {
         let n = num
-        //X0列のY1234を消す
+        //X0列のY1,2,3,4を消す
         for (let y = 1; y < 5; y++) {
             led.unplot(0, y)
         }
-        //
+        //nがマイナスだったらX0列のY2をつける
         if (n < 0) {
             led.plot(0, 2)
-            n *= -1
+            n *= -1　//n　=　n * -1 //nをプラスにする
         }
-        for (let x = 4; x > 0; x--) {
-            let m = n % 10
-            if (m >= 5) {
-                led.plot(x, 0)
-                m -= 5
+        //x4= x3= x2= x1= 
+        for (let x = 4; x > 0; x--) {　//X4,3,2,1
+            let m = n % 10　//m=nを10で割ったあまり
+            if (m >= 5) {   //あまりが5以上だったら
+                led.plot(x, 0)　//Y0列（一番上）のX値をつける
+                m -= 5　//m = m-5 
             } else {
                 led.unplot(x, 0)
             }
--- test.ts
+++ test.ts
@@ -1,8 +1,7 @@
 // tests go here; this will not be compiled when this package is used as a library
-soroban.showNumber(-123)
-led.plotAll()
-for (let y = 1; y < 5; y++) {
-    led.unplot(0, y)
-}
+soroban.showNumber(-3456)
+//led.plotAll()
+//for (let y = 1; y < 5; y++) {
+ //   led.unplot(0, y)}
 
 
